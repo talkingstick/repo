274de3f5f9435dc33b3462c910cc3cca8ebae289
Shadi Kabajah <shadi.kabajah@gmail.com>
2014-10-30 18:06:23 -0700

    add input 2

--- js/InputStart.js
+++ js/InputStart.js
@@ -1,43 +1,7 @@
 
-function userInput () {
-var input = document.createElement("input");
-input.type = "number";
-container.appendChild(input);
-}
 
-function minutesInput () {
-var input = document.createElement("input");
-input.type = "number";
-container.appendChild(input);
-}
-
-$('#myform').append(html.join(''));
-
-function createRadioElement(name, checked) {
-    var radioInput;
-    try {
-        var radioHtml = '<input type="radio" name="' + name + '"';
-        if (checked) {
-            radioHtml += ' checked="checked"';
-        }
-        radioHtml += '/>';
-        radioInput = document.createElement(radioHtml);
-    } catch (err) {
-        radioInput = document.createElement('input');
-        radioInput.setAttribute('type', 'radio');
-        radioInput.setAttribute('name', name);
-        if (checked) {
-            radioInput.setAttribute('checked', 'checked');
-        }
-    }
-
-    return radioInput;
-}
-
-function startButton () {
-var el = document.getElementById("clickMe");
-if (el.addEventListener)
-    el.addEventListener("click", doFunction, false);
-else if (el.attachEvent)
-    el.attachEvent('onclick', doFunction);
+function start () {
+    localStorage.numberOfUsers = document.getElementById("NumberofUsers").value;
+    localStorage.Session = document.getElementById("Session").value;
+    console.log(localStorage);
 }
