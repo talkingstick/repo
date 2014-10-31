e783001fa3c0c37451047d6ceee73fb0af72ab27
Shadi Kabajah <shadi.kabajah@gmail.com>
2014-10-30 19:21:42 -0700

    inputFunction

--- js/InputStart.js
+++ js/InputStart.js
@@ -4,4 +4,11 @@ function start () {
     localStorage.numberOfUsers = document.getElementById("NumberofUsers").value;
     localStorage.Session = document.getElementById("Session").value;
     console.log(localStorage);
+
+    if (document.getElementById("locked").checked == true) {
+    	window.location.replace("locked.html");
+    }
+    if (document.getElementById("unlocked").checked == true) {
+    	window.location.replace("unlocked.html");
+    }
 }
