5097ab6a07517e21baf6192162665156ef5e67fd
shadi <shadi.kabajah@gmail.com>
2014-11-06 19:01:32 -0800

    analytics_changes

--- js/analytics.js
+++ js/analytics.js
@@ -1,9 +1,37 @@
-var mA = parseInt(localStorage.A);
-var mB  = parseInt(localStorage.B);
+var session = parseInt(localStorage.Session);
+console.log(session);
+var mAspoken = (session/2) - parseInt(localStorage.A);
+var mBspoken = (session/2) - parseInt(localStorage.B);
+console.log(mAspoken,mBspoken);
+var mA = mAspoken/(mAspoken+mBspoken)*100;
+var mB = mBspoken/(mAspoken+mBspoken)*100;
+
+console.log(mA,mB);
  
 
  function result () {
-    document.getElementById("sideA").innerHTML = ("A Spoke for: " +   mA + "mins");
-    document.getElementById("sideB").innerHTML = ("B Spoke for: " +   mB + "mins");
-
+    document.getElementById("result").innerHTML = ("A Spoke for: " +   mAspoken + "mins &" + " B Spoke for: " +   mBspoken + "mins");
 }
+
+
+function morris () {
+	Morris.Donut({
+        element: 'graph',
+        data: [
+          {value: mA, label: 'A'},
+          {value: mB, label: 'B'},
+             ],
+        formatter: function (x) { return x + "%"}
+      }).on('click', function(i, row){
+        console.log(i, row);
+      });
+      result();
+}
+
+
+$(function () {
+  eval($('#code').text());
+  prettyPrint();
+});
+
+
