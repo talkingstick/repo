ca9a6a7603a26e1a3b7bacf7e64d01704b6e5e22
shadi <shadi.kabajah@gmail.com>
2014-11-06 18:59:44 -0800

    Revert "analytics_addition"
    This reverts commit b03fd6376454c4112ffdcbc1f7fcdeba45ef2e28.

--- js/analytics.js
+++ js/analytics.js
@@ -1,37 +1,9 @@
-var session = parseInt(localStorage.Session);
-console.log(session);
-var mAspoken = (session/2) - parseInt(localStorage.A);
-var mBspoken = (session/2) - parseInt(localStorage.B);
-console.log(mAspoken,mBspoken);
-var mA = mAspoken/(mAspoken+mBspoken)*100;
-var mB = mBspoken/(mAspoken+mBspoken)*100;
-
-console.log(mA,mB);
+var mA = parseInt(localStorage.A);
+var mB  = parseInt(localStorage.B);
  
 
  function result () {
-    document.getElementById("result").innerHTML = ("A Spoke for: " +   mAspoken + "mins &" + " B Spoke for: " +   mBspoken + "mins");
+    document.getElementById("sideA").innerHTML = ("A Spoke for: " +   mA + "mins");
+    document.getElementById("sideB").innerHTML = ("B Spoke for: " +   mB + "mins");
+
 }
-
-
-function morris () {
-	Morris.Donut({
-        element: 'graph',
-        data: [
-          {value: mA, label: 'A'},
-          {value: mB, label: 'B'},
-             ],
-        formatter: function (x) { return x + "%"}
-      }).on('click', function(i, row){
-        console.log(i, row);
-      });
-      result();
-}
-
-
-$(function () {
-  eval($('#code').text());
-  prettyPrint();
-});
-
-
