e1bee349b09dc15ac560ef1edde5d5df248ecfcf
test <test>
2014-11-17 20:07:46 -0800

    Revert "ding addition"
    This reverts commit cfb6b17991ea9569e75452028623d9e37a8e7c3a.

--- js/jQ.js
+++ js/jQ.js
@@ -1,39 +1,29 @@
 $(document).ready(function () {
 
     $("#startA").click(function () {
-        
-        if (localStorage.timeUpA==0) {
-            startA();
+        startA();
             $('.sideA').css("background-color","#399464");
             $('#startA').css("visibility","hidden");
-        }
-        if (localStorage.timeUpB==0) {
-            $('.sideB').css("background-color","#EEE");
+            $('.sideB').css("background-color","#D7EAE0");
             $('#startB').css("visibility","visible");
-        }
             $('#pause').css("background-color","#EEE");
     });
 
     $("#startB").click(function () {
-        
-        if (localStorage.timeUpA==0) {
-            $('.sideA').css("background-color","#EEE");
-            $('#startA').css("visibility","visible");
-        }
-        if (localStorage.timeUpB==0) {
-            startB();
+        startB();
             $('.sideB').css("background-color","#399464");
             $('#startB').css("visibility","hidden");
-        }
+            $('.sideA').css("background-color","#D7EAE0");
+            $('#startA').css("visibility","visible");
             $('#pause').css("background-color","#EEE");
     });
 
     $("#pause").click(function () {
-        $('.sideA').css("background-color","#E8B2B2");
         $('.sideB').css("background-color","#E8B2B2");
         $('#startB').css("visibility","visible");
-        $('#startA').css("visibility","visible");
+        $('.sideA').css("background-color","#E8B2B2");
         $('#pause').css("background-color","#B20000");
+        $('#startA').css("visibility","visible");
         pause();
     });
 
@@ -62,16 +52,11 @@ $(document).ready(function () {
          location.reload();
     });
 
+    if (localStorage.timeUpA  == 1 ) {
+        $('.sideA').css("background-color","#E8B2B2");
+    }
+    if (localStorage.timeUpB  == 1 ) {
+        $('.sideB').css("background-color","#E8B2B2");
+    }
+
 });
-
-     
-$.fn.timeUp = function(e) { 
-    console.log(localStorage.timeUpB);
-    $(e).css("background-color","#E8B2B2");
-}
-
-$.fn.ding = function() { 
-        var audioElement = document.createElement('audio');
-        audioElement.setAttribute('src', 'http://www.rangde.org/static/bell-ring-01.mp3');
-        audioElement.setAttribute('autoplay', 'autoplay'); 
-}
